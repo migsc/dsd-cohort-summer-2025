c9983f2859c85d36bde8be0070dd4e0875724425
Jose Lopez Renteria <joselukas24@gmail.com>
2025-07-15 21:14:14 -0500

    changed health variable to count

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -1,16 +1,14 @@
 import prisma from "prisma";
 
 export default async function Service() {
-	let health;
+	let count;
 	try {
-		health = await prisma.user.count();
+		count = await prisma.user.count();
 	} catch (err) {
 		console.log(err);
 	}
 
-	console.log(health);
-
-	if (typeof health !== "number") return <div>Database connection: false</div>;
+	if (typeof count !== "number") return <div>Database connection: false</div>;
 
 	return <div>Database connection: true</div>;
 }
