f0111ccb185b135bd2f5eb9a0b575dd0bc68ad3f
Raisa Dorzback <raisa.dorzback@gmail.com>
2025-07-17 23:33:49 -0400

    FEAT: Set up what is needed to use shadcn sidebar

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,62 @@
+import { Calendar, Home, CreditCard, Settings } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Service Catalog",
+    url: "./service",
+    icon: Home,
+  },
+  {
+    title: "My Bookings",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Invoices",
+    url: "#",
+    icon: CreditCard,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
