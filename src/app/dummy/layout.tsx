28699987705b636016b93824d66bf6ec31b5e9fa
Jose Lopez Renteria <joselukas24@gmail.com>
2025-07-18 00:47:51 -0500

    REFACTOR: Removed html&body elements from layout file (CHRT-31)

--- src/app/dummy/layout.tsx
+++ src/app/dummy/layout.tsx
@@ -1,37 +1,29 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "../../styles/globals.css";
 
 const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                <SidebarProvider>
-                    <AppSidebar />
-                    <SidebarTrigger />
-                    <main>
-                        {children}
-                    </main>
-                </SidebarProvider>
-            </body>
-        </html>
-    );
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarTrigger />
+      <main>{children}</main>
+    </SidebarProvider>
+  );
 }
