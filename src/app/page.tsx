16a9924fef1bbf46055ace60b70e85655d871f75
Abiel Ortega <abielortega90@gmail.com>
2025-07-23 01:06:01 -0500

    FEAT: Completed most of landing page, linked up the onboarding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,42 @@
 "use client";
-import Navigation from "@/components/navigation";
 import React from "react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button"
+import {  SquareChevronRight } from "lucide-react";
+import Navigation from "@/components/navigation";
 
-export default function Home() {
-	return (
-		<React.Fragment>
-			<Navigation />
-			<div>This is the homepage</div>
-		</React.Fragment>
-);
+export default function Home(){
+	const router = useRouter();
+	return(
+		<header className="flex gap-7 h-screen flex-col items-center text-black">
+			<nav className="flex justify-between w-full p-4 px-20">
+				<div className="flex items-center justify-center">
+					<img src="/images/small_logo.png" alt="Logo" className="h-10 w-10" />
+					<p className="ml-2 text-2xl font-bold text-blue-600">CleanHub</p>
+				</div>
+				<div className="flex items-center space-x-4 gap-2.5">
+					<ul className="flex space-x-4">
+						<li><a href="/">Contact</a></li>
+					</ul>
+					<span>
+						<button onClick={()=>{router.push("/login")}} className="main_button">Login</button>
+					</span>
+				</div>
+			</nav>
+			<div className="flex flex-1 w-full px-14 max-w-[1800px] mt-[300px]">
+				{/* content */}
+				<div className="w-1/2 h-full max-w-[900px]">
+					<hr className="bg-blue-600 w-[100px] h-1.5" />
+					<h1 className="text-6xl font-black mb-5 mt-3">Your assistant who manages your customers cleaning needs.</h1>
+					<p className="max-w-[700px] leading-8 mb-5">Our app is a comprehensive software platform that empowers cleaning businesses to launch and manage their operations online. From initial setup to customer bookings and payment processing, we provide everything cleaning professionals need to run their business efficiently.</p>
+
+					<a href="./login" className=" flex gap-2.5 w-fit p-4 rounded-xl border bg-white border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white transition-colors duration-300 cursor-pointer">Get Started Managing Your Business <SquareChevronRight /></a>
+				</div>
+				{/* large logo */}
+				<div className="w-1/2 h-full max-w-[900px]">
+					<img src="/images/main_logo.png" alt="main logo of site" />
+				</div>
+			</div>
+		</header>
+	);
 }
