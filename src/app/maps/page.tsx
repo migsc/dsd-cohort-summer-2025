26af86c8f8ac19e65a1acd4f8d8a13da2089c689
demcross-xencom <Demetrius.cross@xencom.com>
2025-07-23 17:02:09 -0500

    Hooked URL params with component

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useState } from "react";
 
 import { Input } from "@/components/ui/input";
@@ -13,34 +11,11 @@ export default function Maps() {
         customerAddress: ""
     };
 
-    const [values, setValues] = useState(initAddresses);
-
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-
-        setValues({
-            ...values,
-            [name]: value,
-        })
-
-    }
 
     return (
         <section className = "space-y-12">
-            <form id = "maps-form" className="w-1/2">
-                <section className="flex flex-col m-4 p-2">
-                    <label htmlFor="worker-input" className="block text-sm/6 font-medium text-gray-100">Worker Address</label>
-                    <Input className={"workerAddressInput"} type= {"text"} id = {"worker-input"} name = {"workerAddress"} value={values.workerAddress} onChange={handleInputChange} />
-
-                    <label htmlFor="customer-input" className="block text-sm/6 font-medium text-gray-100">Customer Address</label>
-                    <Input className={"customerAddressInput"} type= {"text"} id = {"customer-input"} name = {"customerAddress"} value={values.customerAddress} onChange={handleInputChange} />
-                </section>
-                <div className="flex justify-center">
-                    <input className="submit" type="submit" value = "Submit" />
-                </div>
-            </form>
+            
         </section>
     );
-
 
 }
