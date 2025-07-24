a7d509a51b404ba16867e4bef90b6ee94c3ac2f0
Abiel Ortega <abielortega90@gmail.com>
2025-07-23 19:47:23 -0500

    CHORE: fixed the centering of the page and was able to add a small animation for the logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Navigation from "@/components/navigation";
 export default function Home(){
 	const router = useRouter();
 	return(
-		<header className="flex gap-7 h-screen flex-col items-center text-black grid-background">
+		<header className="flex gap-7 w-screen h-screen flex-col items-center text-black grid-background">
 			<nav className="flex justify-between w-full p-4 px-20">
 				<div className="flex items-center justify-center">
 					<img src="/images/small_logo.png" alt="Logo" className="h-10 w-10" />
@@ -23,18 +23,18 @@ export default function Home(){
 					</span>
 				</div>
 			</nav>
-			<div className="flex flex-1 w-full px-14 max-w-[1800px] mt-[300px]">
+			<div className="flex items-center justify-center w-full px-14 max-w-[1800px]">
 				{/* content */}
-				<div className="w-1/2 h-full max-w-[900px]">
-					<hr className="bg-blue-600 w-[100px] h-1.5" />
-					<h1 className="text-6xl font-black mb-5 mt-3">Your assistant who manages your customers cleaning needs.</h1>
+				<div className=" w-1/2 max-w-[900px]">
+					<hr className="bg-blue-600 w-[100px] h-1.5 mb-5 rounded-sm" />
+					<h1 className="text-6xl font-black mb-5 mt-3 uppercase	">Your assistant who manages your customers cleaning needs.</h1>
 					<p className="max-w-[700px] leading-8 mb-5">Our app is a comprehensive software platform that empowers cleaning businesses to launch and manage their operations online. From initial setup to customer bookings and payment processing, we provide everything cleaning professionals need to run their business efficiently.</p>
 
 					<a href="./login" className=" flex gap-2.5 w-fit p-4 rounded-xl border bg-white border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white transition-colors duration-300 cursor-pointer">Get Started Managing Your Business <SquareChevronRight /></a>
 				</div>
 				{/* large logo */}
-				<div className="w-1/2 h-full max-w-[900px]">
-					<img src="/images/main_logo.png" alt="main logo of site" />
+				<div>
+					<img src="/images/main_logo.png" alt="main logo of site" className="floatAnimation hover:" />
 				</div>
 			</div>
 		</header>
