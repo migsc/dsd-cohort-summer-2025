36ed3153e8d05b826b2095a166d1fd5e60598000
Raisa Dorzback <raisa.dorzback@gmail.com>
2025-07-29 13:36:57 -0400

    Add pages for each client portal page

---
+++ src/app/customer/bookings/page.tsx
@@ -0,0 +1,9 @@
+export default function MyBookings() {
+    return (
+        <div className="text-black">
+            <h1 className='text-center font-bold text-3xl my-2'>
+                My Bookings
+            </h1>
+        </div>
+    )
+}
---
+++ src/app/customer/payments/page.tsx
@@ -0,0 +1,9 @@
+export default function Payments() {
+    return (
+        <div className="text-black">
+            <h1 className='text-center font-bold text-3xl my-2'>
+                Payments
+            </h1>
+        </div>
+    )
+}
