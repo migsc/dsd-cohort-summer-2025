d5ef0d3cbf677aff8804606d856d01f3464c4022
tobi <tobi19.py@gmail.com>
2025-07-30 11:55:19 -0500

    FEAT: Create calendar events route

--- src/app/api/business/calendar-events/route.ts
+++ src/app/api/business/calendar-events/route.ts
@@ -1,7 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/lib/auth";
 import prisma from "@/lib/prisma";
-import { getBusinessIdFromUser, getBusinessOperatingHours } from "@/app/business-repo";}
+import {
+  getBusinessIdFromUser,
+  getBusinessOperatingHours,
+} from "@/app/business-repo";
 
 export async function GET(req: NextRequest) {
   try {
@@ -12,16 +15,25 @@ export async function GET(req: NextRequest) {
 
     const businessId = await getBusinessIdFromUser(session?.user?.id);
     if (!businessId) {
-      return NextResponse.json({ error: "Business not found" }, { status: 404 });
+      return NextResponse.json(
+        { error: "Business not found" },
+        { status: 404 }
+      );
     }
     const operatingHours = await getBusinessOperatingHours(businessId);
     if (!operatingHours) {
-      return NextResponse.json({ error: "Operating hours not found" }, { status: 404 });
+      return NextResponse.json(
+        { error: "Operating hours not found" },
+        { status: 404 }
+      );
     }
 
     return NextResponse.json({ operatingHours }, { status: 200 });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
