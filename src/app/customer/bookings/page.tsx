677cb86162356e828a21a7058379a7b85e06575c
Raisa Dorzback <raisa.dorzback@gmail.com>
2025-07-30 20:10:06 -0400

    FEAT: display previous bookings and current bookings (CHRT-45)

--- src/app/customer/bookings/page.tsx
+++ src/app/customer/bookings/page.tsx
@@ -1,13 +1,12 @@
 import {
   Table,
   TableBody,
-  TableCaption,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import { Button } from "@/components/ui/button"
+import { PreviousBookingRow } from '@/components/ui/custom/tableRows'
+import BookingProgressTracker from '@/components/ui/custom/progressTracker'
 
 export default function MyBookings() {
     return (
@@ -16,35 +15,19 @@ export default function MyBookings() {
                 My Bookings
             </h1>
 
-            {/* Current/In Progress Booking */}
-            <h2 className="text-black text-center font-bold text-lg text-gray-600 mb-2">In Progress</h2>
-            <Table className="text-black">
-                <TableHeader className="bg-blue-500">
-                    <TableRow>
-                    <TableHead className="w-[100px]">Order No.</TableHead>
-                    <TableHead>Service</TableHead>
-                    <TableHead>Payment Method</TableHead>
-                    <TableHead className="text-right">Amount</TableHead>
-                    <TableHead className="text-right">Rating</TableHead>
-                    <TableHead className="text-right">Status</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    <TableRow>
-                    <TableCell className="font-medium">1234</TableCell>
-                    <TableCell>Standard House Cleaning</TableCell>
-                    <TableCell>Credit Card</TableCell>
-                    <TableCell className="text-right">$250.00</TableCell>
-                    <TableCell className="text-right">Pending</TableCell>
-                    <TableCell className="text-right">
-                        {/* ***TODO: have this button open a modal to enter new date/time slot */}
-                        <Button className="cursor-pointer">
-                            Rebook
-                        </Button>
-                    </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
+            {/* In Progress Bookings */}
+            <section className="w-full flex justify-center">
+                <div className="w-2xl">
+                    <BookingProgressTracker 
+                        orderNum="001" 
+                        service="Standard House Cleaning" 
+                        amount="$250.00" 
+                        currentStatus="on_way" 
+                        expectedCompletion="2:00 PM Today" 
+                        placedDate="July 25, 2025" 
+                    />
+                </div>
+            </section>
 
             {/* Previous Bookings */}
             <h2 className="text-black text-center font-bold text-lg text-gray-600 mt-4 mb-2">Previous Bookings</h2>
@@ -52,27 +35,16 @@ export default function MyBookings() {
                 <TableHeader className="bg-blue-500">
                     <TableRow>
                     <TableHead className="w-[100px]">Order No.</TableHead>
+                    <TableHead className="w-[100px]">Date Fulfilled</TableHead>
                     <TableHead>Service</TableHead>
-                    <TableHead>Payment Method</TableHead>
-                    <TableHead className="text-right">Amount</TableHead>
-                    <TableHead className="text-right">Rating</TableHead>
-                    <TableHead className="text-right"></TableHead>
+                    <TableHead className="text-left w-[200px]">Payment Method</TableHead>
+                    <TableHead className="text-left w-[120px]">Amount</TableHead>
+                    <TableHead className="text-center w-[120px]">Rating</TableHead>
+                    <TableHead className="text-center w-[120px]">Rebook</TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    <TableRow>
-                    <TableCell className="font-medium">1234</TableCell>
-                    <TableCell>Standard House Cleaning</TableCell>
-                    <TableCell>Credit Card</TableCell>
-                    <TableCell className="text-right">$250.00</TableCell>
-                    <TableCell className="text-right">5 Stars</TableCell>
-                    <TableCell className="text-right">
-                        {/* ***TODO: have this button open a modal to enter new date/time slot */}
-                        <Button className="cursor-pointer">
-                            Rebook
-                        </Button>
-                    </TableCell>
-                    </TableRow>
+                    <PreviousBookingRow orderNum="001" dateFulfilled="07-02-2025" service="Standard House Cleaning" paymentMethod="Credit Card" amount="$250.00"/>
                 </TableBody>
             </Table>
         </div>
