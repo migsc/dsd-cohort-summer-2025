6ccfea67237ccbfa7b30ed4ffef024d6404ba53c
Abiel Ortega <abielortega90@gmail.com>
2025-08-01 16:30:01 -0500

    FEAT: Chagned the theme

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ import SignInForm from "@/components/forms/sign-in-form";
 import SignUpForm from "@/components/forms/sign-up-form";
 
 export default function LoginPage() {
-  const [showSignIn, setShowSignIn] = useState(false);
+  const [showSignIn, setShowSignIn] = useState(true);
 
   return showSignIn ? (
       <SignInForm onSwitchToSignUp={() => setShowSignIn(false)} />
