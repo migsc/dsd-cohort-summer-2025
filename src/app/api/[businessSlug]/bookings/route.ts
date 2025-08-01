4a4a0e9f05e6115466549193613c47f927e4d10b
Jose Lopez Renteria <joselukas24@gmail.com>
2025-07-31 20:20:20 -0500

    FEAT: Add a bookings route behind business slug, updated prisma schemas to include bookins, updated seed to create business/customer profiles each with bookings data

---
+++ src/app/api/[businessSlug]/bookings/route.ts
@@ -0,0 +1,140 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+interface RouteContext {
+  params: Promise<{
+    businessSlug: string;
+  }>;
+}
+
+async function findCustomer(userId: string) {
+  return prisma.customer.findUnique({
+    where: { userId },
+  });
+}
+
+async function findBusiness(userId: string) {
+  return prisma.business.findUnique({
+    where: { userId },
+  });
+}
+
+export async function GET(request: Request, context: RouteContext) {
+  const { businessSlug } = await context.params;
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (!session || !session.user || !session.user.id) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  const userId = session.user.id;
+  try {
+    const businessProfileWithBookings = await prisma.business.findUnique({
+      where: {
+        userId: userId,
+      },
+      include: {
+        bookings: true,
+      },
+    });
+
+    if (!businessProfileWithBookings) {
+      return NextResponse.json(
+        { message: "Business profile not found." },
+        { status: 404 }
+      );
+    }
+
+    console.log(businessProfileWithBookings.bookings);
+    return NextResponse.json(
+      {
+        bookings: businessProfileWithBookings.bookings,
+      },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.log("Error fetching business profile: ", err);
+    return NextResponse.json(
+      { message: "Internal Server Error." },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: Request) {
+  const session = await auth.api.getSession({ headers: await headers() });
+  const formData = await request.json();
+  // TODO: Create booking
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: Promise<{ businessSlug: string }> }
+) {
+  const { businessSlug } = await params;
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (!session || !session.user || !session.user.id) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  // Parse the incoming JSON
+  const body = await req.json();
+  const { bookingId, status } = body;
+
+  const business = await findBusiness(session.user.id);
+  console.log("Business", business);
+
+  if (!business) {
+    return NextResponse.json(
+      { message: "Businiess not found" },
+      { status: 404 }
+    );
+  }
+
+  // Validate required field
+  if (!bookingId) {
+    return NextResponse.json(
+      { message: "Booking ID is required" },
+      { status: 400 }
+    );
+  }
+  // Check if the booking exists and belongs to this customer
+  const booking = await prisma.booking.findUnique({
+    where: { id: bookingId },
+  });
+
+  if (!booking) {
+    return NextResponse.json({ message: "Booking not found" }, { status: 404 });
+  }
+
+  console.log(booking.businessId, business.id);
+  if (booking.businessId !== business.id) {
+    return NextResponse.json(
+      { message: "You are not authorized to update this booking" },
+      { status: 403 }
+    );
+  }
+
+  // Perform the update
+  try {
+    const updatedBooking = await prisma.booking.update({
+      where: { id: bookingId },
+      data: {
+        status: status.toUpperCase(),
+      },
+    });
+
+    return NextResponse.json(updatedBooking, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Error updating booking" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
--- src/app/customer/page.tsx
+++ src/app/customer/page.tsx
@@ -1,48 +1,55 @@
-import ServicesGrid from '@/components/ui/custom/servicesgrid';
-import { Suspense } from 'react';
-import { Skeleton } from '@/components/ui/skeleton';
-import SearchBar from '@/components/ui/custom/searchbar';
-import { getBusinessWithServices } from '@/lib/services';
+import ServicesGrid from "@/components/ui/custom/servicesgrid";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import SearchBar from "@/components/ui/custom/searchbar";
+import { getBusinessWithServices } from "@/lib/services";
 
 export default async function OurServices(props: {
-    searchParams?: Promise<{
-        query?: string;
-        page?: string;
-    }>;
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
 }) {
-    const searchParams = await props.searchParams;
-    const query = searchParams?.query || '';
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
 
-    // ***TODO: fetch businessId from database. hardcoded right now
-    const businessId = '6888eda1126c69ebb2d37bf2';
-    
-    // Fetch business info & services from database
-    const businessData = await getBusinessWithServices(businessId);
+  // ***TODO: fetch businessId from database. hardcoded right now
+  const businessId = "6888eda1126c69ebb2d37bf2";
 
-    if (!businessData) {
-        return (
-            <div>
-                <h1 className='text-center font-bold text-3xl my-2'>Our Services</h1>
-                <div>Business not found</div>
-            </div>
-    );
-    }
+  // Fetch business info & services from database
+  const businessData = await getBusinessWithServices(businessId);
 
+  if (!businessData) {
     return (
-        <section className='mx-1 sm:mx-10'>
-            <h1 className='text-center font-bold text-3xl my-2 text-black'>Our Services</h1>
-            <h2 className='text-center text-2xl mt-2 mb-4 text-black'>{businessData.businessName}</h2>
-            {!query && (
-                <p className='text-center text-black'>
-                    {businessData.businessDescription}
-                </p>
-            )}
-            <div className='w-full flex justify-center my-5'>
-                <SearchBar placeholder='Search services...'/>
-            </div>
-            <Suspense key={query} fallback={<Skeleton className='h-4 w-[250px]' />}>
-                <ServicesGrid services={businessData.services} query={query}></ServicesGrid>
-            </Suspense>
-        </section>
+      <div>
+        <h1 className="my-2 text-center text-3xl font-bold">Our Services</h1>
+        <div>Business not found</div>
+      </div>
     );
-};
+  }
+
+  return (
+    <section className="mx-1 sm:mx-10">
+      <h1 className="my-2 text-center text-3xl font-bold text-black">
+        Our Services
+      </h1>
+      <h2 className="mb-4 mt-2 text-center text-2xl text-black">
+        {businessData.businessName}
+      </h2>
+      {!query && (
+        <p className="text-center text-black">
+          {businessData.businessDescription}
+        </p>
+      )}
+      <div className="my-5 flex w-full justify-center">
+        <SearchBar placeholder="Search services..." />
+      </div>
+      <Suspense key={query} fallback={<Skeleton className="h-4 w-[250px]" />}>
+        <ServicesGrid
+          services={businessData.services}
+          query={query}
+        ></ServicesGrid>
+      </Suspense>
+    </section>
+  );
+}
